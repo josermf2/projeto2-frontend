5dfd3f91a7ad1db9543fe954406c39bd7054d452
josermf2 <josermf2@al.insper.edu.br>
2021-11-08 21:50:00 -0300

    finaliza arquivo views

--- backend/favorites/models.py
+++ backend/favorites/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class User(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
     password = models.CharField(max_length=200)
 
@@ -8,9 +9,8 @@ class User(models.Model):
         return "{}".format(self.name)
 
 class Favorite (models.Model):
-    id = models.AutoField(primary_key=True)
-    tournament = models.CharField(max_length=200)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tournament = models.CharField(max_length=200)
 
     def __str__(self):
         return "{0}.{1}".format(self.user, self.tournament)
--- backend/favorites/urls.py
+++ backend/favorites/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('api/notes/<int:note_id>/', views.api_note),
-    # Você possivelmente tem outras rotas aqui.
+    path('api/user/<str:user_name>/', views.api_get_user),
+    path('api/user', views.api_post_user),
+    path('api/favorite/<str:user_name>/', views.api_get_favorite),
+    path('api/favorite', views.api_post_favorite),    
+    path('api/favorite/delete', views.api_delete_favorite),    
 ]
