bf5fb2c815dbfa48d652fa2d44f9d1d19d906496
luccabvs <seu@email.com>
2021-11-16 01:38:13 -0300

    small fix

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
-    headers: {'X-Auth-Token': '004303a5a43a4ec9b36923c47a490493'}
+    headers: process.env.REACT_APP_TOKEN_API
 });
 
 export default api;
