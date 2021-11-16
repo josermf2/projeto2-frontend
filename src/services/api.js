a7b455b75523f28716da7c4fe629cd0c9ccc5a93
luccabvs <seu@email.com>
2021-11-16 01:48:42 -0300

    smal fix

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
-    headers: process.env.REACT_APP_TOKEN_API
+    headers: {'X-Auth-Token':process.env.REACT_APP_TOKEN_API}
 });
 
 export default api;
